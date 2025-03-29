7566ff73df777644861555fe426ac5fc10d17f1b
daDude123 <153016828+Coding-master5@users.noreply.github.com>
2025-03-28 21:01:39 -0400

    Update process.js

--- process.js
+++ process.js
@@ -1,3 +1,3 @@
 function runPhp(e) {
-  window.open(`https://coding-master5.github.io/php.js/output.php?q=${e}`);
+  var win = window.open(`https://coding-master5.github.io/php.js/output.php?q=${e}`)
 }
